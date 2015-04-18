d96afba4edd4e776d3bafb7610d2dae7240e829c
Dave Canam <david.canam@gmail.com>
2015-04-18 00:28:05 -0300

    Initial Commit

--- src/js/load.js
+++ src/js/load.js
@@ -1,6 +1,11 @@
+// var tileSize = 64,
+var tileSize = 16,
+rows = 10,
+cols = 12;
+
 var Game = {
-  w: 800,
-  h: 600
+  w: tileSize * cols,
+  h: tileSize * rows
 };
 
 // var w = 800;
@@ -13,10 +18,23 @@ Game.Boot = function(game) {
 Game.Boot.prototype = {
   preload: function() {
     // console.log('blah'+Game.w);
-		this.game.stage.backgroundColor = '#FFF';
+		this.game.stage.backgroundColor = '#ececec';
 		this.game.load.image('loading', 'assets/images/loading.png');
 		this.game.load.image('title', 'assets/images/title.png');
 		this.game.load.image('instructions', 'assets/images/instructions.png');
+
+    this.game.renderer.renderSession.roundPixels = true;
+
+    //Automatically Scale
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.maxHeight = window.innerHeight;
+    this.game.scale.maxWidth = window.innerHeight*(Game.w/Game.h);
+
+    this.game.stage.scale.pageAlignHorizontally = true;
+    this.game.stage.scale.pageAlignVertically = true;
+    this.game.scale.setScreenSize(true);
+
+
   },
   create: function() {
    this.game.state.start('Load');
@@ -31,13 +49,18 @@ Game.Load.prototype = {
   preload: function() {
     
     //Debug Plugin
-    // this.game.add.plugin(Phaser.Plugin.Debug);
+    this.game.add.plugin(Phaser.Plugin.Debug);
 
     //Loading Screen Message/bar
     var loadingText = this.game.add.text(Game.w, Game.h, 'Loading...', { font: '30px Helvetica', fill: '#000' });
   	loadingText.anchor.setTo(0.5, 0.5);
   	var preloading = this.game.add.sprite(Game.w/2-64, Game.h/2+50, 'loading');
   	this.game.load.setPreloadSprite(preloading);
+
+    this.game.load.spritesheet('tiles', 'assets/images/tiles.png', 16, 16, 4);
+    this.game.load.image('player', 'assets/images/player.png');
+    this.game.load.image('shadow', 'assets/images/shadow.png');
+
 
     // Music Track
     // this.game.load.audio('music','soundtrack.mp3');
--- src/js/play.js
+++ src/js/play.js
@@ -26,6 +26,19 @@ Game.Play.prototype = {
   create: function() {
     this.game.world.setBounds(0, 0 ,Game.w ,Game.h);
 
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.player = this.game.add.sprite(Game.w/2, Game.h/2, 'player')
+    this.game.physics.arcade.enable(this.player)
+
+    this.shadow = this.game.add.sprite(Game.w/2+64, Game.h/2, 'shadow')
+    this.game.physics.arcade.enable(this.shadow)
+
+    this.playerSpeed = 50;
+    this.shadowSpeed = 150;
+    this.meditating = false;
+
+
     // // Music
     // this.music = this.game.add.sound('music');
     // this.music.volume = 0.5;
@@ -38,9 +51,53 @@ Game.Play.prototype = {
     dKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
     // muteKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
 
+    spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
   },
 
   update: function() {
+
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+
+    this.shadow.body.velocity.x = 0;
+    this.shadow.body.velocity.y = 0;
+    // spaceKey.onDown.add(function() {
+    //   this.meditating = !this.meditating;
+    // }, this);
+
+    if (spaceKey.isDown && this.switching === false) {
+      this.meditating = !this.meditating;
+      this.switching = true;
+    }
+    spaceKey.onUp.add(function() {
+      this.switching = false;
+    },this);
+
+
+    if (this.meditating) {
+      if (wKey.isDown) {
+        this.shadow.body.velocity.y = -this.shadowSpeed;
+      }else if (sKey.isDown) {
+        this.shadow.body.velocity.y = this.shadowSpeed;
+      }else if (aKey.isDown) {
+        this.shadow.body.velocity.x = -this.shadowSpeed;
+      }else if (dKey.isDown) {
+        this.shadow.body.velocity.x = this.shadowSpeed;
+      }
+    }else {
+       if (wKey.isDown) {
+        this.player.body.velocity.y = -this.playerSpeed;
+      }else if (sKey.isDown) {
+        this.player.body.velocity.y = this.playerSpeed;
+      }else if (aKey.isDown) {
+        this.player.body.velocity.x = -this.playerSpeed;
+      }else if (dKey.isDown) {
+        this.player.body.velocity.x = this.playerSpeed;
+      }     
+    }    
+
+
 
     // // Toggle Music
     // muteKey.onDown.add(this.toggleMute, this);
@@ -56,7 +113,7 @@ Game.Play.prototype = {
   //   }
   // },
   // render: function() {
-  //   game.debug.text('Health: ' + tri.health, 32, 96);
+  //   this.game.debug.text('Meditating: ' + this.meditating, 32, 96);
   // }
 
 };
