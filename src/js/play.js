5a74d02c866ff8ef4c31fbfcc835908b2f8bb174
Dave Canam <david.canam@gmail.com>
2015-04-18 02:19:56 -0300

    refactored player info into its own class

--- src/js/play.js
+++ src/js/play.js
@@ -13,11 +13,6 @@
 // var musicOn = true;
 
 
-var wKey;
-var aKey;
-var sKey;
-var dKey;
-
 Game.Play = function(game) {
   this.game = game;
 };
@@ -39,20 +34,7 @@ Game.Play.prototype = {
     console.log(this.map.getObjectIndex('start'))
     console.log(this.map.getObjectIndex('end'))
 
-    // this.map.createFromObjects('objects', 4, 'player', 0, true, false);
-
-    this.player = this.game.add.sprite(0, 0, 'player')
-    this.game.physics.arcade.enable(this.player)
-    this.player.body.collideWorldBounds = true;
-
-    this.shadow = this.game.add.sprite(0, 0, 'shadow')
-    this.shadow.alpha = 0;  
-    this.game.physics.arcade.enable(this.shadow)
-    this.shadow.body.collideWorldBounds = true;
-
-    this.playerSpeed = 50;
-    this.shadowSpeed = 150;
-    this.meditating = false;
+    this.player = new Player(this.game);
 
 
     // // Music
@@ -60,68 +42,12 @@ Game.Play.prototype = {
     // this.music.volume = 0.5;
     // this.music.play('',0,1,true);
 
-    //Setup WASD and extra keys
-    wKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
-    aKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
-    sKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
-    dKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
-    // muteKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
-
-    spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-
-
 
   },
 
   update: function() {
-    this.game.physics.arcade.collide(this.player, this.layer);
-    this.game.physics.arcade.collide(this.shadow, this.layer);
 
-    this.player.body.velocity.x = 0;
-    this.player.body.velocity.y = 0;
-
-    this.shadow.body.velocity.x = 0;
-    this.shadow.body.velocity.y = 0;
-
-    if (spaceKey.isDown && this.switching === false) {
-      this.meditating = !this.meditating;
-      this.switching = true;
-    }
-    spaceKey.onUp.add(function() {
-      this.switching = false;
-      if (this.meditating) {
-        this.shadow.alpha = 1;
-        this.shadow.x = this.player.x;
-        this.shadow.y = this.player.y;
-      }else {
-        this.shadow.alpha = 0;
-      }
-    },this);
-
-
-    if (this.meditating) {
-      if (wKey.isDown) {
-        this.shadow.body.velocity.y = -this.shadowSpeed;
-      }else if (sKey.isDown) {
-        this.shadow.body.velocity.y = this.shadowSpeed;
-      }else if (aKey.isDown) {
-        this.shadow.body.velocity.x = -this.shadowSpeed;
-      }else if (dKey.isDown) {
-        this.shadow.body.velocity.x = this.shadowSpeed;
-      }
-    }else {
-       if (wKey.isDown) {
-        this.player.body.velocity.y = -this.playerSpeed;
-      }else if (sKey.isDown) {
-        this.player.body.velocity.y = this.playerSpeed;
-      }else if (aKey.isDown) {
-        this.player.body.velocity.x = -this.playerSpeed;
-      }else if (dKey.isDown) {
-        this.player.body.velocity.x = this.playerSpeed;
-      }     
-    }    
-
+    this.player.movements(this.layer);
 
     // // Toggle Music
     // muteKey.onDown.add(this.toggleMute, this);
@@ -136,8 +62,9 @@ Game.Play.prototype = {
   //     this.music.volume = 0.5;
   //   }
   // },
-  // render: function() {
+  render: function() {
+    // this.game.debug.body(this.player);
   //   this.game.debug.text('Meditating: ' + this.meditating, 32, 96);
-  // }
+  }
 
 };
--- src/js/player.js
+++ src/js/player.js
@@ -1,1 +1,87 @@
+var wKey;
+var aKey;
+var sKey;
+var dKey;
 
+
+var Player = function(game) {
+  this.game = game;
+  this.ninja = null;
+  this.shadow = null;
+  this.ninjaSpeed = 75;
+  this.shadowSpeed = 150;
+  this.meditating = false;
+  this.ninja = this.game.add.sprite(0, 0, 'player');
+  this.game.physics.arcade.enable(this.ninja);
+  this.ninja.body.collideWorldBounds = true;
+
+  this.shadow = this.game.add.sprite(0, 0, 'shadow');
+  this.shadow.alpha = 0;  
+  this.game.physics.arcade.enable(this.shadow);
+  this.shadow.body.collideWorldBounds = true;
+
+  //Setup WASD and extra keys
+  wKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
+  aKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+  sKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
+  dKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+  // muteKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
+
+  spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+
+};
+
+Player.prototype = {
+  movements: function(layer) {
+
+    this.game.physics.arcade.collide(this.ninja, layer);
+    this.game.physics.arcade.collide(this.shadow, layer);
+
+    this.ninja.body.velocity.x = 0;
+    this.ninja.body.velocity.y = 0;
+
+    this.shadow.body.velocity.x = 0;
+    this.shadow.body.velocity.y = 0;
+
+    if (spaceKey.isDown && this.switching === false) {
+      this.meditating = !this.meditating;
+      this.switching = true;
+    }
+    spaceKey.onUp.add(function() {
+      this.switching = false;
+      if (this.meditating) {
+        this.shadow.alpha = 1;
+        this.shadow.x = this.ninja.x;
+        this.shadow.y = this.ninja.y;
+      }else {
+        this.shadow.alpha = 0;
+      }
+    },this);
+
+
+    if (this.meditating) {
+      if (wKey.isDown) {
+        this.shadow.body.velocity.y = -this.shadowSpeed;
+      }else if (sKey.isDown) {
+        this.shadow.body.velocity.y = this.shadowSpeed;
+      }else if (aKey.isDown) {
+        this.shadow.body.velocity.x = -this.shadowSpeed;
+      }else if (dKey.isDown) {
+        this.shadow.body.velocity.x = this.shadowSpeed;
+      }
+    }else {
+       if (wKey.isDown) {
+        this.ninja.body.velocity.y = -this.ninjaSpeed;
+      }else if (sKey.isDown) {
+        this.ninja.body.velocity.y = this.ninjaSpeed;
+      }else if (aKey.isDown) {
+        this.ninja.body.velocity.x = -this.ninjaSpeed;
+      }else if (dKey.isDown) {
+        this.ninja.body.velocity.x = this.ninjaSpeed;
+      }     
+    }    
+
+
+  }
+};
